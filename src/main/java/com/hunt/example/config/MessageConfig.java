034b305be1d36fec396085f12a87e7d5f2a0ccea
HunterLyu <shiyu.lv@prudential.com.sg>
2023-07-27 15:47:23 +0800

    update config

--- src/main/java/com/hunt/example/config/MessageConfig.java
+++ src/main/java/com/hunt/example/config/MessageConfig.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MessageConfig {
 
-    public static final String TEST_FLOWABLE = "task1FinishChannel";
+    public static final String TEST_FLOWABLE = "task1FinishTopic";
 
-    // 创建一个名为testtopic的Topic并设置分区数为8，分区副本数为2
+    // 创建一个名为topic的Topic并设置分区数为8，分区副本数为2
     @Bean
     public NewTopic initialTopic() {
         return new NewTopic(TEST_FLOWABLE, 2, (short) 2);
