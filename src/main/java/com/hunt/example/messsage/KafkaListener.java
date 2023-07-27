aa82432e9cbf6a2366178ce74ab41c5e670b820b
HunterLyu <shiyu.lv@prudential.com.sg>
2023-07-27 15:46:01 +0800

    move msg listener.

--- src/main/java/com/hunt/example/messsage/KafkaListener.java
+++ src/main/java/com/hunt/example/messsage/KafkaListener.java
@@ -1,24 +1,16 @@
-package com.hunt.example.service;
+package com.hunt.example.messsage;
 
-import com.hunt.example.config.MessageConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
-
 @Component
-public class KafkaListerner {
+public class KafkaListener {
     protected Logger log = LoggerFactory.getLogger(getClass());
 
 
-    @KafkaListener(topics = {"task1FinishTopic", "task1SendTopic", "task2FinishTopic", "task2SendTopic",
+    @org.springframework.kafka.annotation.KafkaListener(topics = {"task1FinishTopic", "task1SendTopic", "task2FinishTopic", "task2SendTopic",
             "task3SendTopic", "task3FinishTopic"})
     public void onMessage1(ConsumerRecord<?, ?> record) {
         // 消费的哪个topic、partition的消息,打印出消息内容
