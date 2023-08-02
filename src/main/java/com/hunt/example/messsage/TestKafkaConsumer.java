626d6c2b19914bd9183df47d59331292d225f67c
HunterLyu <shiyu.lv@prudential.com.sg>
2023-08-02 09:32:39 +0800

    update consume code

--- src/main/java/com/hunt/example/messsage/TestKafkaConsumer.java
+++ src/main/java/com/hunt/example/messsage/TestKafkaConsumer.java
@@ -3,18 +3,17 @@ package com.hunt.example.messsage;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class KafkaListener {
+public class TestKafkaConsumer {
     protected Logger log = LoggerFactory.getLogger(getClass());
 
 
-    @org.springframework.kafka.annotation.KafkaListener(topics = {"task1FinishTopic", "task1SendTopic", "task2FinishTopic", "task2SendTopic",
+    @KafkaListener(topics = {"task1FinishTopic", "task1SendTopic", "task2FinishTopic", "task2SendTopic",
             "task3SendTopic", "task3FinishTopic"})
     public void onMessage1(ConsumerRecord<?, ?> record) {
-        // 消费的哪个topic、partition的消息,打印出消息内容
-        System.out.println("简单消费：" + record.topic() + "-" + record.partition() + "-" + record.value());
-
+        log.info("consume message of:：" + record.topic() + "-" + record.partition() + "-" + record.value());
     }
 }
